7fd0a4c006ff696c820419ba93ae29a5b674c7a0
Jordan Sewell <jrsewell400@gmail.com>
2020-01-14 17:57:55 -0700

    Add instance variables and attr_reader

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -2,8 +2,12 @@ require_relative 'key'
 require_relative 'offset'
 
 class Encrypt
+  attr_reader :message, :key, :date
 
   def initialize(message, key, date)
+    @message = message
+    @key = key
+    @date = date
   end
 
 end
