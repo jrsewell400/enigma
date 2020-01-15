f7b5642c44d2ced2b8fe973075a289c25ea93cc2
Jordan Sewell <jrsewell400@gmail.com>
2020-01-15 06:26:49 -0700

    Add string to integer converter helper method for generating final offset, also add @offset instance variable to instantiate new Offset class

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -6,8 +6,14 @@ class Encrypt
 
   def initialize(message, key, date)
     @message = message
+    @offset = Offset.new
     @key = key
     @date = date
   end
 
+  def string_to_int_converter(hash_to_convert)
+    hash_to_convert.each do |key, value|
+      hash_to_convert[key] = value.to_i
+    end
+  end
 end
